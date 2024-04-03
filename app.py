e376c70a465d6201d4efd35642ec249813821462
Sang Marie Colley <oboy2010@hotmail.com>
2024-04-03 14:56:35 +0100

    Added local database connection to make application dynamic

--- app.py
+++ app.py
@@ -1,48 +1,17 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
-    'salary': 'Rs. 10,00,000'
-  },
-  {
-    'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Delhi, India',
-    'salary': 'Rs. 15,00,000'
-  },
-  {
-    'id': 3,
-    'title': 'Frontend Engineer',
-    'location': 'Remote'
-  },
-  {
-    'id': 4,
-    'title': 'Backend Engineer',
-    'location': 'San Francisco, USA',
-    'salary': '$120,000'
-  },
-  {
-    'id': 5,
-    'title': 'Software Engineer',
-    'location': 'San Francisco, USA',
-    'salary': '$110,000'
-  }
-]
-
 @app.route("/")
 def hello():
-    return render_template("home.html", 
-                           jobs=JOBS)
+    job_list = load_jobs_from_db()
+    return render_template("home.html", jobs=job_list)
 
 @app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    job_list = load_jobs_from_db()
+    return jsonify(job_list)
 
 
 if __name__ == '__main__':
---
+++ database.py
@@ -0,0 +1,31 @@
+from sqlalchemy import create_engine, text
+import os
+db_username = os.getenv("DATABASE_USERNAME")
+db_password = os.getenv("DATABASE_PASSWORD")
+db_server = os.getenv("DATABASE_SERVER")
+
+engine = create_engine(f"mysql+pymysql://{db_username}:{db_password}@{db_server}/joviancareers?charset=utf8mb4")
+
+# with engine.connect() as conn:
+#         sql_query = 'SELECT * FROM jobs'
+#         results = conn.execute(text(sql_query))
+#         result_all = results.all()
+#         d = []
+#         for column in result_all:
+#             res = column._mapping
+#             d.append(dict(res))
+#         print(d)
+
+
+
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        sql_query = 'SELECT * FROM jobs'
+        results = conn.execute(text(sql_query))
+        result_all = results.all()
+
+        result_dicts = []
+        for row in result_all:
+            res = row._mapping
+            result_dicts.append(dict(res))
+        return result_dicts            
